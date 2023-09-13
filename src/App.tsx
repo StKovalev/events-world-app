3004a6153541845a89b678ac68c749c36b630933
Stanislav <vipersteel@192.168.1.8>
2023-09-13 20:33:52 +0300

    add react-router-dom & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link, NavLink, Routes, Route }  from 'react-router-dom';
+
+import { About, Contacts, EventsCatalog, Main, NotFound } from './pages'
+import { RequireAuth } from './hocs/requireAuth'
+
 import './App.css';
 // import Login from '../src/pages/login/login'
-import {Counter, ProductCard, User, BlogPost, Post } from '../src/pages/test'
+// import {Counter, ProductCard, User, BlogPost, Post } from '../src/pages/test'
+
 // import {UserContext} from './contexts/User'
 
 // const User = {
@@ -9,17 +15,53 @@ import {Counter, ProductCard, User, BlogPost, Post } from '../src/pages/test'
 //   name: '',
 // }
 
+const ROUTES = {
+  main: '/',
+  catalog: '/catalog',
+  about: '/about',
+  contacts: '/contacts',
+}
+
 function App() {
+
+  // TODO : доработать стили навигации
   return (
 
         <div className="App">
-          <header className="App-header">
-            {/* <Counter /> */}
-            {/* <ProductCard id={''}/> */}
-            {/* <User /> */}
-            {/* <BlogPost /> */}
-            <Post />
+          <header className="App-header1">
+            <div>logo</div>
+            <nav>
+              <ul> 
+                <li><NavLink to={ROUTES.main} style={({isActive}) => ({color: isActive ? "green" : "dark"})}>Главная</NavLink></li>
+                <li><NavLink to={ROUTES.catalog} style={({isActive}) => ({color: isActive ? "green" : "dark"})}>Каталог событий</NavLink></li>
+                <li><NavLink to={ROUTES.about} style={({isActive}) => ({color: isActive ? "green" : "dark"})}>О проекте</NavLink></li>
+                <li><NavLink to={ROUTES.contacts} style={({isActive}) => ({color: isActive ? "green" : "dark"})}>Контакты</NavLink></li>
+              </ul>
+            </nav>
+            <div>
+              <span>login / logout</span>
+              <span>IconIser</span>
+            </div>
           </header>
+          <main>
+            <Routes>
+              <Route path={ROUTES.main} element={<Main/>} /> 
+              <Route path={ROUTES.catalog} element={
+                <RequireAuth>
+                  <EventsCatalog/>
+                </RequireAuth>
+              } />
+              <Route path={ROUTES.about} element={<About/>} /> 
+              <Route path={ROUTES.contacts} element={<Contacts/>} />
+              <Route path='*' element={<NotFound/>} /> 
+            </Routes>
+          </main>
+          <ul> 
+            <li><Link to={ROUTES.main}>Главная</Link></li>
+            <li><Link to={ROUTES.catalog}>Каталог событий</Link></li>
+            <li><Link to={ROUTES.about}>О проекте</Link></li>
+            <li><Link to={ROUTES.contacts}>Контакты</Link></li>
+          </ul>
         </div>
   );
 }
