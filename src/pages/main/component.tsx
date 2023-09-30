8e3cf4d30d83e5399c2889a3915d1fccf7914011
Stanislav <vipersteel@192.168.1.8>
2023-09-30 12:11:51 +0300

    add Redux Query

--- src/pages/main/component.tsx
+++ src/pages/main/component.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useGetUsersQuery } from '../../services/users'
+
 
 export const Main: React.FC = () => {
+    const { data, error, isLoading, isSuccess, isError } = useGetUsersQuery()
+
 
     return (
         <div>
             <h1>Main</h1>
+            {isLoading &&  'Загрузка ...'}
+            {isError && error.message}
+            {isSuccess && data &&
+                data.map((user: any) => <h2 key={user.id}>{user.name}</h2>)
+            }
         </div>
     )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { configureStore } from '@reduxjs/toolkit' 
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { usersApi } from '../services/users'
 
-import taskReducer from '../reducers/taskReducer'
+export const store = configureStore({
+    reducer: {
+        [usersApi.reducerPath]: usersApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware)
+})
 
-export const store = createStore(
-    taskReducer,
-    composeWithDevTools(applyMiddleware(thunk))
-)
+setupListeners(store.dispatch)
+
+// import { createStore, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+
+// import taskReducer from '../reducers/taskReducer'
+
+// export const store = createStore(
+//     taskReducer,
+//     composeWithDevTools(applyMiddleware(thunk))
+// )
